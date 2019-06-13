70b7862df471bc3855bd0467fb6506116042d064
Aaron Simmons <paleozogt@gmail.com>
2019-06-13 14:28:30 -0600

    REFLECT_DECL has all const permutations

--- src/reflect.hpp
+++ src/reflect.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
-#define REFLECT_DECL(clazz)                     \
-    template<typename F>                        \
-    friend void reflect(clazz &, const F &);    \
-                                                \
-    template<typename F>                        \
-    friend void reflect(const clazz &, F &);    \
+#define REFLECT_DECL(clazz)                           \
+    template<typename F>                              \
+    friend void reflect(clazz &, const F &);          \
+                                                      \
+    template<typename F>                              \
+    friend void reflect(const clazz &, F &);          \
+                                                      \
+    template<typename F>                              \
+    friend void reflect(const clazz &, const F &);    \
+                                                      \
+    template<typename F>                              \
+    friend void reflect(clazz &, F &);                \
 
