9e655106d75011cfa4c6e51992c76d9fc4e87541
Aaron Simmons <paleozogt@gmail.com>
2019-06-19 15:51:42 -0600

    is_reflectable type trait

--- src/reflect.hpp
+++ src/reflect.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <string>
+#include <functional>
+#include <type_traits>
+
 #define REFLECT_DECL(clazz)                                   \
     template<typename T, typename F>                          \
     static void reflect(T &o, F &f);                          \
@@ -7,3 +11,42 @@
     template<typename T1, typename T2, typename F>            \
     static void reflect(T1 &o1, T2 &o2, F &f);                \
 
+
+namespace reflect {
+    class NoopCallback {
+    public:
+        template <typename A>
+        void operator()(const std::string &, const std::string &, const A &a) const {
+        }
+
+        template <typename A>
+        void operator()(const std::string &, const std::string &, const A &a, const A &b) const {
+        }
+    };
+
+    template<typename C, typename F = NoopCallback>
+    using reflect_fp = decltype(
+                            std::function<void(C&, F&)>(
+                                &C::template reflect<C,F>
+                            )
+                        );
+
+    template<typename C, typename C2 = C, typename F = NoopCallback>
+    using reflect2_fp = decltype(
+                            std::function<void(C&, C2&, F&)>(
+                                &C::template reflect<C,C2,F>
+                            )
+                        );
+
+    template <typename T>
+    class is_reflectable {
+        template <typename C>
+        static std::true_type test(reflect_fp<C>, reflect2_fp<C>);
+
+        template <typename C>
+        static std::false_type test(...);
+
+    public:
+        enum { value = decltype(test<T>(nullptr, nullptr))::value };
+    };
+}
---
+++ test/ClangReflectTests.cpp
@@ -0,0 +1,10 @@
+#include "Foobar.hpp"
+#include "FoobarReflect.hpp"
+
+#include "gtest/gtest.h"
+
+TEST(clangReflect, reflectable) {
+    ASSERT_TRUE(reflect::is_reflectable<example::foo::Foobase>::value);
+    ASSERT_TRUE(reflect::is_reflectable<example::foo::Foobar>::value);
+    ASSERT_FALSE(reflect::is_reflectable<std::string>::value);
+}
